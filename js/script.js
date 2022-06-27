fccd5956341b49632442b994f3978e1a211240f7
NicoloYan <nico.yan@outlook.it>
2022-06-27 16:45:32 +0200

    Milestone 3 complete

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-// Rifare l'esercizio della to do list.
-// Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
-// - text, una stringa che indica il testo del todo
-// - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
-// MILESTONE 1
-// Stampare all'interno di una lista, un item per ogni todo.
-// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
-// MILESTONE 2
-// Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
-// MILESTONE 3
-// Predisporre un campo di input testuale e un pulsante "aggiungi": 
-// cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, 
-// che quindi viene aggiunto alla lista dei todo esistenti.
 
 var app = new Vue(
     {
@@ -34,12 +21,17 @@ var app = new Vue(
         },
         methods: {
             // Funzione che, utilizzando la tecnica dello splice, 
-            // rimuove dall'array l'elemento selezionato con la X.
+            // rimuove dall'array originale l'elemento selezionato con la X.
             removeTask(index) {
                 this.todos.splice(index, 1);
             },
+            // Funzione che aggiunge un nuovo oggetto all'array originale e, in seguito, svuota la input.
             addTask() {
-                
+                this.todos.push({
+                    text: this.newTaskText,
+                    done: false
+                })
+                this.newTaskText = ''
             }    
         }
         
